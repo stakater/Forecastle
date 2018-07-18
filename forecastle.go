28ff52c9fba28606f3dfc2b36e0470d0208c3b5c
Waseem Hassan <waseemhassanshahid@gmail.com>
2018-07-18 12:56:59 +0500

    Serve static files using packr

--- forecastle.go
+++ forecastle.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	"github.com/gobuffalo/packr"
 	"github.com/gorilla/mux"
 	"github.com/stakater/Forecastle/pkg/handlers"
 	"github.com/stakater/Forecastle/pkg/log"
@@ -20,6 +21,10 @@ func main() {
 
 	router.Path("/apps").Queries("namespaces", "{namespaces}").HandlerFunc(handlers.AppsHandler)
 	router.Path("/apps/").Queries("namespaces", "{namespaces}").HandlerFunc(handlers.AppsHandler)
+
+	// Serve static files using packr
+	box := packr.NewBox("./static")
+	router.PathPrefix("/").Handler(http.StripPrefix("/", http.FileServer(box)))
 
 	logger.Info("Listening at port 8000")
 	if err := http.ListenAndServe(":8000", router); err != nil {

7aeb09ee5dcc3521158b45bc439d6cb9f782f7d1
Waseem Hassan <waseemhassanshahid@gmail.com>
2018-11-05 15:36:13 +0500

    Fix test case

--- pkg/kube/wrappers/ingress_test.go
+++ pkg/kube/wrappers/ingress_test.go
@@ -29,7 +29,7 @@ func TestNewIngressWrapper(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := NewIngressWrapper(tt.args.ingress); !reflect.DeepEqual(got, tt.want) {
+			if got := NewIngressWrapper(tt.args.ingress, ""); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("NewIngressWrapper() = %v, want %v", got, tt.want)
 			}
 		})

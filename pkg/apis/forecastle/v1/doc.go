c227d62cd9c5b09728286ba5375240b9bc027106
waseem <waseemhassanshahid@gmail.com>
2019-10-10 13:01:49 +0200

    Add apis for ForecastleApp CRD

---
+++ pkg/apis/forecastle/v1/doc.go
@@ -0,0 +1,5 @@
+// Package v1 contains API Schema definitions for the forecastle v1 API group
+// +k8s:deepcopy-gen=package,register
+// +k8s:defaulter-gen=TypeMeta
+// +groupName=forecastle.stakater.com
+package v1

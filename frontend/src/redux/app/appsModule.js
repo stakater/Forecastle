b3050c3ced63585f38741cddd6299556a49a2fb9
Zadkiel AHARONIAN <hello@zadkiel.fr>
2024-11-09 19:21:03 +0100

    Update appsModule.js

--- frontend/src/redux/app/appsModule.js
+++ frontend/src/redux/app/appsModule.js
@@ -44,9 +44,9 @@ const loadApps = () => async dispatch => {
     dispatch(loading());
     let { data } = await getApps();
 
-    const apps = sortAlphabetically(data, i => i.name)
+    const apps = groupBy("group")(sortAlphabetically(data, i => i.name))
     const groups = sortAlphabetically(uniq(data.map(i => i.group)))
- 
+
     dispatch(loadAppsSuccess({ groups, apps ));
   } catch (e) {
     if (e.response && e.response.data) {
